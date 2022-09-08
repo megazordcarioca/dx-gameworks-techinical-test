9c582ef94e23852de4094c56555d636b30da5b4c
Joey <josiasdsj1@gmail.com>
2022-09-07 22:44:09 -0300

    First commit

---
+++ Source/EHT01_BP/EHT01_BP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EHT01_BP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EHT01_BP, "EHT01_BP" );
